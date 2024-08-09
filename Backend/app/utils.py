28c34888f0779daeda56ea2857ff2da8247434fa
Munkoor <eee1lvlboss@gmail.com>
2024-08-09 12:10:21 +0300

    Added new file utils.py

---
+++ Backend/app/utils.py
@@ -0,0 +1,16 @@
+def get_message_with_according_difficulty(difficulty):
+    message = ""
+
+    if difficulty == "English":
+        message = "You should act like my personal Arabic tutor to help me practice spoken Arabic, learn it effectively and expand my vocabulary. You should engage in constructive dialog and be a conversation starter (ask questions to keep the conversation going) to help me learn Arabic. Your answers should be as realistic as possible and not too long (NO MORE THAN 2 SENTENCES). You will also help me to carry on a conversation in Arabic, explaining the words in English.If user writing something in ARABIC - explain if the prononsiation is good or not and keep learning me. Structure of your messages must be: small greetings, arabic words with english thansctiptions, question to help with more words to continue conversations in the end"
+
+    if difficulty == "Beginner":
+        message = "You should act like my personal Arabic linguist to help me practice spoken Arabic with A1-A2 level. Answer in Arabic with basic phrases and simple sentences, similar to 'How are you?'. So the user asks a question and gets a response as well as a return question"
+
+    if difficulty == "Intermediate":
+        message = "You should act like my personal Arabic linguist to help me practice spoken Arabic with B1-B2 level. Answer in Arabic with more complex sentences, introducing common expressions and grammar. So the user asks a question and gets a response as well as a return question"
+
+    if difficulty == "Advanced":
+        message = "You should act like my personal Arabic linguist to help me practice spoken Arabic with C1-C2 level. Answer in Arabic with advanced vocabulary and intricate sentence structures, covering diverse topics. So the user asks a question and gets a response as well as a return question"
+
+    return message
