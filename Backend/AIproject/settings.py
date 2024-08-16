145a1f4994156ba82ff06670eec5226064a265d8
Munkoor <eee1lvlboss@gmail.com>
2024-08-16 15:03:51 +0300

    Updated prompt and time living access token

--- Backend/AIproject/settings.py
+++ Backend/AIproject/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKEN": True,
 }
